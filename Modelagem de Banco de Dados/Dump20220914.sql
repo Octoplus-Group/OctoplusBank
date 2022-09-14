86b7fb5e99b1caecfbe4b66579dcd6475f959777
zani19 <thiago.zani@uol.com.br>
2022-09-14 14:42:34 -0300

    Create Dump20220914.sql
    Backup (DUMP) do Banco de dados com as seguintes tabelas:
    Banco
    Agencia
    Conta
    Cliente
    Transação

---
+++ Modelagem de Banco de Dados/Dump20220914.sql
@@ -0,0 +1,170 @@
+CREATE DATABASE  IF NOT EXISTS `octoplus_bank` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `octoplus_bank`;
+-- MySQL dump 10.13  Distrib 5.7.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: octoplus_bank
+-- ------------------------------------------------------
+-- Server version	5.7.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `agencia`
+--
+
+DROP TABLE IF EXISTS `agencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `agencia` (
+  `ID_BANCO` int(11) DEFAULT NULL,
+  `ID_AGENCIA` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID_AGENCIA`),
+  KEY `ID_BANCO` (`ID_BANCO`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `agencia`
+--
+
+LOCK TABLES `agencia` WRITE;
+/*!40000 ALTER TABLE `agencia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `agencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `banco`
+--
+
+DROP TABLE IF EXISTS `banco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `banco` (
+  `ID_BANCO` int(11) NOT NULL AUTO_INCREMENT,
+  `NOME_BANCO` varchar(20) DEFAULT NULL,
+  `CAPITAL_TOTAL` float DEFAULT NULL,
+  PRIMARY KEY (`ID_BANCO`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banco`
+--
+
+LOCK TABLES `banco` WRITE;
+/*!40000 ALTER TABLE `banco` DISABLE KEYS */;
+INSERT INTO `banco` VALUES (1,'OCTOPLUS_BANK',1000000);
+/*!40000 ALTER TABLE `banco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `ID_CONTA` int(11) DEFAULT NULL,
+  `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT,
+  `NOME` varchar(255) NOT NULL,
+  `CPF` char(11) NOT NULL,
+  `DATA_NASCIMENTO` date NOT NULL,
+  `GENERO` enum('Masculino','Feminino','Outro') NOT NULL,
+  `TELEFONE` varchar(15) NOT NULL,
+  `CEP` varchar(8) NOT NULL,
+  `ENDERECO` varchar(255) NOT NULL,
+  `CIDADE` varchar(255) NOT NULL,
+  `BAIRRO` varchar(255) NOT NULL,
+  `NUMERO` varchar(10) NOT NULL,
+  `SENHA` char(6) NOT NULL,
+  PRIMARY KEY (`ID_CLIENTE`),
+  UNIQUE KEY `CPF` (`CPF`),
+  KEY `ID_CONTA` (`ID_CONTA`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `conta`
+--
+
+DROP TABLE IF EXISTS `conta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conta` (
+  `ID_BANCO` int(11) DEFAULT NULL,
+  `ID_AGENCIA` int(11) DEFAULT NULL,
+  `ID_CONTA` int(11) NOT NULL AUTO_INCREMENT,
+  `DATA_ABERTURA` datetime NOT NULL,
+  `TIPO` enum('CORRENTE','POUPANÇA') NOT NULL,
+  `SALDO` float DEFAULT '0',
+  PRIMARY KEY (`ID_CONTA`),
+  KEY `ID_BANCO` (`ID_BANCO`),
+  KEY `ID_AGENCIA` (`ID_AGENCIA`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conta`
+--
+
+LOCK TABLES `conta` WRITE;
+/*!40000 ALTER TABLE `conta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transacoes`
+--
+
+DROP TABLE IF EXISTS `transacoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transacoes` (
+  `ID_CONTA` int(11) DEFAULT NULL,
+  `ID_TRANSACAO` int(11) NOT NULL AUTO_INCREMENT,
+  `TIPO` enum('DEPOSITO','SAQUE') DEFAULT NULL,
+  `DATA` datetime DEFAULT NULL,
+  `VALOR` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID_TRANSACAO`),
+  KEY `ID_CONTA` (`ID_CONTA`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transacoes`
+--
+
+LOCK TABLES `transacoes` WRITE;
+/*!40000 ALTER TABLE `transacoes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transacoes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-14 14:39:24
