b183d11e224876f345270637fe04dac37b895a0a
zani19 <thiago.zani@uol.com.br>
2022-09-15 10:50:31 -0300

    Dump - Removendo Foreignkey
    Removido cave estrangeira (ID_BANCO) em ID_CONTA

--- Modelagem de Banco de Dados/Dump20220915.sql
+++ Modelagem de Banco de Dados/Dump20220915.sql
@@ -110,14 +110,12 @@ DROP TABLE IF EXISTS `conta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `conta` (
-  `ID_BANCO` int(11) DEFAULT NULL,
   `ID_AGENCIA` int(11) DEFAULT NULL,
   `ID_CONTA` int(11) NOT NULL AUTO_INCREMENT,
   `DATA_ABERTURA` datetime NOT NULL,
   `TIPO` enum('CORRENTE','POUPANÇA') NOT NULL,
   `SALDO` float DEFAULT '0',
   PRIMARY KEY (`ID_CONTA`),
-  KEY `ID_BANCO` (`ID_BANCO`),
   KEY `ID_AGENCIA` (`ID_AGENCIA`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -167,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-14 14:39:24
+-- Dump completed on 2022-09-15 10:48:40
