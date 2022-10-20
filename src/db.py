9495671b822fc987fc918f1109e474d06b2ce390
IgorKenzoMS <igor.sasaki@fatec.sp.gov.br>
2022-10-20 09:15:14 -0300

    Edição de dados pelo gerente
    Adicionei a opção de edição de dados pelo gerente de agência e aproveitei para mexer em algumas rotas

--- src/db.py
+++ src/db.py
@@ -1,3 +1,3 @@
 import pymysql
 
-mysql =pymysql.connect(host='localhost', port=3306, user='root', passwd='Topsp808!@', database='octoplus_bank')
+mysql =pymysql.connect(host='localhost', port=3306, user='root', passwd='fatec', database='octoplus_bank')
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -3,9 +3,7 @@ from src.controllers.erros import NotFoundController
 
 routes ={
     "index_route":"/","indexcontroller":IndexController.as_view("index"),
-    "index_admin_route":"/gerenteagencia","indexgacontroller":IndexController.as_view("indexga"),
     "not_found_route":404,"not_found_controller": NotFoundController.as_view("not_found"),
-    "delete_route":"/delete/cliente/<int:id>","delete_controller":DeleteClienteController.as_view("delete"),
     "update_route":"/update/cliente/<int:id>","update_controller":UpdateClienteController.as_view("update"),
     "cadastro_route":"/cadastro_cliente","cadastro_cliente_controller":CadastroClienteController.as_view("cadastro"),
     "login_route":"/login_cliente","login_controller":LoginController.as_view("login"),
@@ -17,24 +15,27 @@ routes ={
     "pagina_saque_route":"/pagina_saque/cliente/<int:id>","pagina_saque_controller":paginaSaqueController.as_view("paginasaque"),
     "realizar_saque_route":"/realizar_saque/cliente/<int:id>","realizar_saque_controller":realizarSaqueController.as_view("realizarsaque"),
     "home_user_id_route":"/home_user/<int:id>","home_user_id_controller":HomeUserIDController.as_view("homeuserid"),
-    "home_gerente_agencia_id_route":"/home_gerente_agencia/<int:id>","home_gerente_agencia_id_controller":HomeGerenteAgenciaController.as_view("homegaid"),
-    "link_gerenciar_contas":"/link_gerenciar_contas/<int:id>","link_gerenciar_contas_controller":LinkGerenciarContasController.as_view("linkgcontas"),
-    "aprovar_contas":"/aprovar_contas/<int:id>","aprovar_contas_controller":AprovacaoContaController.as_view("aprovacaodeconta"),
-    "link_modo_gerente_contas":"/link_modo_gerente_contas/<int:id>","modo_gerente_contas_controller":ModoGerenteAgenciaController.as_view("modogerente"),
-    "link_aprovacao_deposito":"/link_aprovacao_deposito/<int:id>","link_aprovacao_deposito_controller":LinkAprovacaoDepositoController.as_view("linkaprovacaodeposito"),
-    "aprovacao_deposito":"/aprovacao_deposito/<int:id>","aprovacao_deposito_controller":ExecucaoDepositoController.as_view("aprovacaodeposito"),
     "link_extrato":"/link_extrato/<int:id>","link_extrato_controller":LinkExtratoController.as_view("linkextrato"),
     "gerar_extrato":"/gerar_extrato/<int:id>","gerar_extrato_controller":GerarExtratoController.as_view("gerarextrato"),
-    "pagina_deposito_gerente_route":"/pagina_deposito_gerente/cliente/<int:id>","pagina_deposito_gerente_controller":paginaDepositoGerenteController.as_view("paginadepositogerente"),
-    "realizar_deposito_gerente_route":"/realizar_gerente_deposito/cliente/<int:id>","realizar_deposito_gerente_controller":realizarDepositoGerenteController.as_view("realizardepositogerente"),
-    "pagina_saque_gerente_route":"/pagina_gerente_saque/cliente/<int:id>","pagina_gerente_saque_controller":paginaSaqueGerenteController.as_view("paginasaquegerente"),
-    "realizar_saque_gerente_route":"/realizar_saque_gerente/cliente/<int:id>","realizar_saque_gerente_controller":realizarSaqueGerenteController.as_view("realizarsaquegerente"),
-    "home_gerente_conta":"/home_gerente_conta/<int:id>","home_gerente_conta_controller":HomeGerenteContaController.as_view("homegerenteconta"),
     "link_cadastro":"/link_cadastro/","link_cadastro_controller":LinkPaginaCadastroController.as_view("linkcadastro"),
     "cadastro_execucao":"/cadastro_execucao/","cadastro_execucao_controller":CadastroClienteController.as_view("cadastrocliente"),
     "verificar_aprovacao":"/verificaraprovacao/","verificar_aprovacao_controller":VerificacaoAprovacao.as_view("verificaraprovacao"),
     "solicitacao_deletar":"/solicitacaodeletar/<int:id>","solicitacao_deletar_controller":DeleteClienteRequisicaoController.as_view("solicitacaodeletar"),
 
+    #gerete
+    "index_admin_route":"/gerenteagencia","indexgacontroller":IndexController.as_view("indexga"),
+    "home_gerente_agencia_id_route":"/home_gerente_agencia/<int:id>","home_gerente_agencia_id_controller":HomeGerenteAgenciaController.as_view("homegaid"),
+    "home_gerente_conta":"/home_gerente_conta/<int:id>","home_gerente_conta_controller":HomeGerenteContaController.as_view("homegerenteconta"),
+    "pagina_deposito_gerente_route":"/pagina_deposito_gerente/cliente/<int:id>","pagina_deposito_gerente_controller":paginaDepositoGerenteController.as_view("paginadepositogerente"),
+    "realizar_deposito_gerente_route":"/realizar_gerente_deposito/cliente/<int:id>","realizar_deposito_gerente_controller":realizarDepositoGerenteController.as_view("realizardepositogerente"),
+    "pagina_saque_gerente_route":"/pagina_gerente_saque/cliente/<int:id>","pagina_gerente_saque_controller":paginaSaqueGerenteController.as_view("paginasaquegerente"),
+    "realizar_saque_gerente_route":"/realizar_saque_gerente/cliente/<int:id>","realizar_saque_gerente_controller":realizarSaqueGerenteController.as_view("realizarsaquegerente"),
+    "link_modo_gerente_contas":"/link_modo_gerente_contas/<int:id>","modo_gerente_contas_controller":ModoGerenteAgenciaController.as_view("modogerente"),
+    "link_gerenciar_contas":"/link_gerenciar_contas/<int:id>","link_gerenciar_contas_controller":LinkGerenciarContasController.as_view("linkgcontas"),
+    "aprovar_contas":"/aprovar_contas/<int:id>","aprovar_contas_controller":AprovacaoContaController.as_view("aprovacaodeconta"),
+    "link_aprovacao_deposito":"/link_aprovacao_deposito/<int:id>","link_aprovacao_deposito_controller":LinkAprovacaoDepositoController.as_view("linkaprovacaodeposito"),
+    "aprovacao_deposito":"/aprovacao_deposito/<int:id>","aprovacao_deposito_controller":ExecucaoDepositoController.as_view("aprovacaodeposito"),
+    "delete_route":"/delete/cliente/<int:id>","delete_controller":DeleteClienteController.as_view("delete"),
 }
 
 
