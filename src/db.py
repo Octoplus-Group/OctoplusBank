efd1c19f959c733fc3a60f8034e0c11bb2e66669
Jean <dev.jeejor@gmail.com>
2022-11-04 22:56:50 -0300

    Erro ao logar resolvido, só resta arrumar conflitos com CSS para as páginas alinharem.

--- src/db.py
+++ src/db.py
@@ -1,7 +1,3 @@
 import pymysql
 
-<<<<<<< HEAD
-mysql =pymysql.connect(host='localhost', port=3306, user='root', passwd='gabes', database='octoplus_bank')
-=======
 mysql =pymysql.connect(host='localhost', port=3306, user='root', passwd='fatec', database='octoplus_bank')
->>>>>>> de241c16a326638ff1cac95bda28e83cd8812494
