a27601b4d42ea9dd405ed8ac21499960eb2dfd35
Thiago Zani <thiago.zani@uol.com.br>
2022-11-27 08:49:56 -0300

    Add logo tela cadastro GG

--- src/db.py
+++ src/db.py
@@ -1,7 +1,3 @@
 import pymysql
 
-<<<<<<< HEAD
-mysql =pymysql.connect(host='localhost', port=3306, user='root', passwd='gabes', database='octoplus_bank')
-=======
 mysql =pymysql.connect(host='localhost', port=3306, user='root', passwd='', database='octoplus_bank')
->>>>>>> b243ec9701be36f277becb0b9152f866fe01802c
